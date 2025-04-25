3e00f70c33ab1885d0b4e5ff39d6f157042a75b2
Farouk MOHAMED <kayson91@gmail.com>
2025-04-25 14:38:29 +0000

    update and manage multi sounds

--- lib/audio/data/audio_repository_impl.dart
+++ lib/audio/data/audio_repository_impl.dart
@@ -1,14 +1,20 @@
+import 'dart:math';
+
 import 'package:flame_audio/flame_audio.dart';
 import '../domain/audio_repository.dart';
 
 class AudioRepositoryImpl implements AudioRepository {
   bool _muted = false;
+  final List<String> _bgmTracks = ['music.mp3', 'music.mp3', 'music.mp3'];
+
+  final Random _random = Random();
 
   @override
   Future<void> playBackgroundMusic() async {
     if (_muted) return;
     await FlameAudio.bgm.initialize();
-    await FlameAudio.bgm.play('music/music.mp3');
+    final track = _bgmTracks[_random.nextInt(_bgmTracks.length)];
+    await FlameAudio.bgm.play("music/$track");
   }
 
   @override
